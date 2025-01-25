4400409532d55cc1b9390d84c5aa73ef3901208e
Shubham53Mishra <21cse53@vmkvec.edu.in>
2025-01-25 14:36:13 +0530

    config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const config = {
   darkMode: ["class"],
   content: [
     './pages/**/*.{js,jsx}',
@@ -74,4 +74,6 @@ module.exports = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
+
+export default config;
